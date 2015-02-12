8f9b2f791589c637fd4df85d4597e12c77b1ac28
Erric <erricgunawan@gmail.com>
2015-02-12 17:13:20 +0700

    remove title on post format Link

--- content-link.php
+++ content-link.php
@@ -17,33 +17,21 @@ do_atomic( 'before_entry' ); // live-wire_before_entry ?>
 
 	<?php if ( is_singular() ) { ?>
 
-		<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title]' ); ?>
-
-		<?php echo apply_atomic_shortcode( 'byline', '<div class="byline">' . __( '[post-format-link] published on [entry-published] [entry-comments-link before=" | "] [entry-edit-link before=" | "]', 'live-wire' ) . '</div>' ); ?>
-
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'live-wire' ), 'after' => '</p>' ) ); ?>
 		</div><!-- .entry-content -->
 
+		<?php echo apply_atomic_shortcode( 'byline', '<div class="byline">' . __( '[post-format-link] published on [entry-published] [entry-comments-link before=" | "] [entry-edit-link before=" | "]', 'live-wire' ) . '</div>' ); ?>
+
 		<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="Tagged "]', 'live-wire' ) . '</div>' ); ?>
 
 	<?php } else { ?>
 	
-		<?php if ( get_the_title() ) { ?>
-
-			<?php if ( current_theme_supports( 'get-the-image' ) ) get_the_image( array( 'meta_key' => 'Thumbnail', 'size' => 'live-wire-thumbnail' ) ); ?>
-
-			<h2 class="entry-title"><a href="<?php echo esc_url( live_wire_url_grabber() ); ?>" title="<?php the_title_attribute(); ?>"><?php printf( '%s <span class="meta-nav">&rarr;</span>', the_title( '', '', false ) ); ?></a></h2>
-
-		<?php } else { ?>
-
-			<div class="entry-content">
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'live-wire' ), 'after' => '</p>' ) ); ?>
-			</div><!-- .entry-content -->
-
-		<?php } ?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'live-wire' ), 'after' => '</p>' ) ); ?>
+		</div><!-- .entry-content -->
 
 		<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[post-format-link] published on [entry-published] [entry-permalink before="| "] [entry-comments-link before="| "] [entry-edit-link before="| "]', 'live-wire' ) . '</div>' ); ?>
 		
